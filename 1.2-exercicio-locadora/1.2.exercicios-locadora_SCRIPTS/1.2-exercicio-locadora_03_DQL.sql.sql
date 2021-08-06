49b42a60c87f4b6262c3d1dc481b75138b6ffdcf
Vinicius <vini.evangelista@live.com>
2021-08-05 23:21:17 -0300

    DQL feitos

---
+++ 1.2-exercicio-locadora/1.2.exercicios-locadora_SCRIPTS/1.2-exercicio-locadora_03_DQL.sql.sql
@@ -0,0 +1,38 @@
+INSERT INTO EMPRESA (nomeEmpresa)
+VALUES ('eita'),('localiza');
+GO
+
+
+INSERT INTO CLIENTE (nomeCliente, cpf)
+VALUES ('Siverino', 97392487898),('Gilberto', 77049587);
+GO
+
+
+INSERT INTO MARCA (nomeMarca)
+VALUES ('fiat'),('VW');
+GO
+
+INSERT INTO MODELO (nomeModelo, idMarca)
+VALUES ('Gol', 1),('Sedan',1);
+GO
+
+INSERT INTO CARRO (placaCarro, idEmpresa, idModelo)
+VALUES ('1211000',1,3);
+GO
+
+INSERT INTO ALUGUEL (idCarro, idCliente, dataAluguel )
+VALUES (7,1, '10/09/2021');
+GO
+
+SELECT * FROM CARRO;
+GO
+SELECT * FROM MODELO;
+GO
+SELECT * FROM EMPRESA;
+GO
+SELECT * FROM CLIENTE;
+GO
+SELECT * FROM ALUGUEL;
+GO
+
+
